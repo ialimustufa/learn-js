d9f29e0f5bed9d0fa93161f056e59c00db26775e
sheetalbania <139550713+sheetalbania@users.noreply.github.com>
2024-06-12 10:11:14 +0530

    Add files via upload

---
+++ student-projects/day1/script.js
@@ -0,0 +1,185 @@
+
+/*
+console.log("hello from script");
+var name="sheetal bania";
+let age=21;
+const pi = 3.14;
+console.log(name);
+console.log(age);
+console.log(pi);
+let person = null;
+let isStudent = true;
+let userData = "tcet";
+console.log(person);
+console.log(isStudent);
+console.log(userData);
+
+let sum=5 + 5;
+let name1="sheetal " + "asmita";
+console.log(sum);
+console.log(name1);
+
+let isEqual = 5 == "5";
+console.log(isEqual);
+
+let newName = "sheetal";
+let age1 = 21;
+let greeting = `hello my name is ${newName} and i am ${age} years old`;
+console.log(greeting);
+
+let multiLineString = `Welcome to the world of indian web series. enjoy the show!`;
+console.warn(multiLineString);
+console.error(multiLineString);
+
+let arr = [1,2,3];
+let obj = {name:"sheetal",class:"AI&DS-A",rollno:05};
+console.log(obj);
+*/
+/*
+let a=10;
+let b=30;
+
+let add = a+b;
+console.log(sum);
+let sub = a-b;
+console.log(sub);
+let mul = a*b;
+console.log(mul);
+let div = a/b;
+console.log(div);
+
+x=10;
+x += 5;
+console.log(`x+=5:${x}`);
+
+x *= 5;
+console.log(`x*=5:${x}`);
+
+x -= 3;
+console.log(`x-=3:${x}`);
+
+x %= 5;
+console.log(`x%=5:${x}`);
+
+x /= 4;
+console.log(`x/=4:${x}`);
+
+console.log(`typeof str:${typeof isStudent}`);
+
+let date = new Date();
+console.log(date);
+console.log(`${date}`);
+
+//alert("Welcome to Javascript Course");
+//console.log(alert);
+
+//let myname = prompt("what is your name?");
+//console.log("employee name is "+myname);
+*/
+/*
+console.table([{name:"sheetal",class:"AI&DS-A",rollno:05},{name:"aarya",class:"AI&DS-A",rollno:22}]);
+*/
+/*
+let workHours = prompt("enter work hours:");
+workHours = parseInt(workHours);
+if (workHours>8){
+    console.log("great job");
+} else if(workHours==8){
+    console.log("completed work");
+} else{
+    console.error("get a new job");
+}
+
+switch (workHours){
+    case 6:
+        console.log("undertime work");
+        break;
+    case 8:
+        console.log("standard work hours");
+        break;
+    case 10:
+        console.log("overtime work hours");
+        break;
+    default:
+        console.log("varied work hours");
+}
+*/
+/*
+for(let i=0;i<=5;i++){
+    console.log("task "+i+" complete panchayat duty");
+}
+*/
+/*
+let i=0;
+while(i<=5){
+    console.log("task "+i+" review gram sabha decisions");
+    i++;
+}
+*/
+/*
+let j=0;
+do{
+    console.log("task "+j+" review gram sabha decisions");
+    j++;
+}while(j<=5);
+*/
+/*
+let directors = ["anurag kashyap","rohit shetty","sanjay leela bhansali","r.madhavan"];
+for(let director of directors){
+    console.log(director);
+}
+*/
+/*
+let actor = {firstName:"kartik",lastname:"aryan",age:30};
+for(let key in actor){
+    console.log(`${key}:${actor[key]}`);
+}
+*/
+/*
+function greet(name){
+    return "hello "+ name + "! ready for the next panchayat task?";
+}
+let message = greet("sheetal");
+console.log(message);
+*/
+/*
+function add(a,b){
+    return a+b;
+}
+let sum = add(5,10);
+console.log(sum);
+*/
+/*
+const greet = function(name){
+    console.log("hello,"+ name+"!");
+};
+greet("sheetal");
+*/
+/*
+const greet = (name) => console.log("hello,"+name+"!");
+*/
+/*
+const add = (x,y) => x+y;
+console.log(add(5,10));
+*/
+
+function changeContent(){
+    document.getElementById("demo").innerHTML="content updated";
+}
+
+function changeStyle(){
+    document.getElementById("demo").style.color="blue";
+    document.getElementById("demo").style.fontSize="30px";
+}
+
+function changeAttribute(){
+    document.getElementById("demo").setAttribute("class","highlight");
+}
+
+//create a dropdown and when u select a particular text it shoul display a message 
+
+//enter two numbers 
+
+
+
+
