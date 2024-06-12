580d14090fb97a2189495b7eb4af7f9a51adf32b
rohandol112 <rohan45321dol@gmail.com>
2024-06-12 13:04:56 +0530

    Day 1

---
+++ student-projects/day1/10_E&CS_Rohan/script.js
@@ -0,0 +1,61 @@
+let currentOperand = '';
+let previousOperand = '';
+let operation = null;
+
+const displayElement = document.getElementById('display');
+
+function clearDisplay() {
+    currentOperand = '';
+    previousOperand = '';
+    operation = null;
+    updateDisplay();
+}
+
+function appendNumber(number) {
+    if (number === '.' && currentOperand.includes('.')) return;
+    currentOperand = currentOperand.toString() + number.toString();
+    updateDisplay();
+}
+
+function chooseOperation(op) {
+    if (currentOperand === '') return;
+    if (previousOperand !== '') {
+        compute();
+    }
+    operation = op;
+    previousOperand = currentOperand;
+    currentOperand = '';
+}
+
+function compute() {
+    let computation;
+    const prev = parseFloat(previousOperand);
+    const current = parseFloat(currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (operation) {
+        case '+':
+            computation = prev + current;
+            break;
+        case '-':
+            computation = prev - current;
+            break;
+        case '*':
+            computation = prev * current;
+            break;
+        case '/':
+            computation = prev / current;
+            break;
+        default:
+            return;
+    }
+    currentOperand = computation;
+    operation = null;
+    previousOperand = '';
+    updateDisplay();
+}
+
+function updateDisplay() {
+    displayElement.innerText = currentOperand;
+}
+
+clearDisplay();
