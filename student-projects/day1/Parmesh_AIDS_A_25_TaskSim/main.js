6e39ba646f822c86ce2ecd4e22bfc6a2f9203bba
Parmesh214 <parmesh.u.gupta@gmail.com>
2024-06-12 11:25:41 +0530

    Parmesh_AIDS_25

---
+++ student-projects/day1/Parmesh_AIDS_A_25_TaskSim/main.js
@@ -0,0 +1,29 @@
+function startTask(){
+    let character = document.getElementById("Character").value;
+
+    let task;
+    switch(character){
+        case "Vegeta":
+            task = "Vegeta: You say I’m arrogant, I say damn right, That’s pride, Pride in the Saiyan I am.";
+            break;
+        
+        case "Itachi":
+            task = "Itachi: One's Reality Might Be Another's Illusion.";
+            break;
+
+        case "Aizen":
+            task = "Aizen: Welcome To My Soul Society";
+            break;
+
+        case "Zaemon":
+            task = "Buri Buri Zaemon: I am always of winning side";
+            break;
+        default :
+        "Select Proper character"
+    }
+
+    document.getElementById("task").innerHTML = task;
+    document.getElementById("Good Luck").innerHTML = Lightspeed;
+
+    document.getElementById("task").classList.add("highlight");
+}
