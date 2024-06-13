d9803ef7e9d8082f4bd2a2bba7b08eabd059b449
Shaikh Ali Mustufa <alimustafaats24@gmail.com>
2024-06-13 22:28:45 +0530

    add day 4 content

---
+++ Day 4/EventHandlingExamples/app.js
@@ -0,0 +1,14 @@
+// App.js file
+
+import React from 'react';
+import EventHandlingExamples from './EventHandlingExamples';
+
+function App() {
+  return (
+    <div className="App">
+      <EventHandlingExamples />
+    </div>
+  );
+}
+
+export default App;
