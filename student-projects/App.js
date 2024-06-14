f197dfab1bdc3d30e1a5f74d88b1ef465606fd55
sheetalbania <139550713+sheetalbania@users.noreply.github.com>
2024-06-14 15:46:03 +0530

    Add files via upload

---
+++ student-projects/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {useState} from "react";
+import axios from 'axios';
+import SearchBar from "./SearchBar";
+import VideoList from "./VideoList";
+import './App.css';
+
+
+const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
+
+function App(){
+  const [videos, setVideos] = useState([]);
+  const onTermSubmit = async (term) => {
+    const response = await axios.get('https://www.googleapis.com/youtube/v3/search',{ 
+      params:{
+        part: 'snippet',
+        maxResults: 5,
+        key: API_KEY,
+        q: term,
+      },
+  });
+  setVideos(response.data.items);
+  };
+
+  return(
+    <div className="ui container">
+      <SearchBar onFormSubmit={onTermSubmit} />
+      <VideoList videos={videos} />
+    </div>
+  )
+
+}
+export default App;
