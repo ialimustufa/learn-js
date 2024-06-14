0194e836016d1f589d6e7b59e84607f1a3ede48a
roshani9324 <roshanimaurya.in@gmail.com>
2024-06-14 16:50:00 +0530

    Add files via upload

---
+++ student-projects/day4/Roshanimaurya-52-A-AI&DS-Day-4/VideoItem.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactPlayer  from "react-player";
+
+function VideoItem({video}){
+    const videoUrl=`https://youtbue.com/watch?v=${video.id.videoId}`;
+    return(
+        <div className="item video-item">
+            <ReactPlayer url={videoUrl} controls={true} className="mb-4" />
+            <div className="content">
+                <div className="header">{ video.snippet.title}
+
+                </div>
+            </div>
+        </div>
+    );
+}
+export default VideoItem;
